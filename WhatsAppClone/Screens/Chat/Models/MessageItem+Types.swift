ba79e3e3a86591fefe872d5f43b43fbc4f53f51e
tmrff <thomasdroff@gmail.com>
2024-07-28 21:57:22 +1200

    Add reaction picker

--- WhatsAppClone/Screens/Chat/Models/MessageItem+Types.swift
+++ WhatsAppClone/Screens/Chat/Models/MessageItem+Types.swift
@@ -7,6 +7,35 @@
 
 import Foundation
 
+enum Reaction: Int {
+    case like
+    case heart
+    case laugh
+    case shocked
+    case sad
+    case pray
+    case more
+    
+    var emoji: String {
+        switch self {
+        case .like:
+            return "👍"
+        case .heart:
+            return "❤️"
+        case .laugh:
+            return "😂"
+        case .shocked:
+            return "😮"
+        case .sad:
+            return "😢"
+        case .pray:
+            return "🙏"
+        case .more:
+            return "+"
+        }
+    }
+}
+
 enum AdminMessageType: String {
     case channelCreation
     case memberAdded
