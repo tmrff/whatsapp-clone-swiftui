d7de8783821b25f37dcc14fdf3e5a7ca6ab32d8d
tmrff <thomasdroff@gmail.com>
2024-07-29 00:00:06 +1200

    Add message menu

--- WhatsAppClone/Screens/Chat/Models/MessageItem+Types.swift
+++ WhatsAppClone/Screens/Chat/Models/MessageItem+Types.swift
@@ -7,6 +7,27 @@
 
 import Foundation
 
+enum MessageMenuAction: String, CaseIterable, Identifiable {
+    case reply, forward, copy, delete
+    
+    var id: String {
+        return rawValue
+    }
+    
+    var systemImage: String {
+        switch self {
+        case .reply:
+            return "arrowshape.turn.up.left"
+        case .forward:
+            return "paperplane"
+        case .copy:
+            return "doc.on.doc"
+        case .delete:
+            return "trash"
+        }
+    }
+}
+
 enum Reaction: Int {
     case like
     case heart
