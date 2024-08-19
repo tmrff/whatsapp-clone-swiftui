0f01a609e349f3c1ccc65c1f9773af28d1c8e713
tmrff <thomasdroff@gmail.com>
2024-08-19 16:19:49 +1200

    Attach reactions and menu to focused message

--- WhatsAppClone/Screens/Chat/Views/MessageListController.swift
+++ WhatsAppClone/Screens/Chat/Views/MessageListController.swift
@@ -45,7 +45,9 @@ final class MessageListController: UIViewController {
     private var blurView: UIVisualEffectView?
     private var focusedView: UIView?
     private var highlightedCell: UICollectionViewCell?
-    
+    private var reactionHostVC: UIViewController?
+    private var messageMenuHostVC: UIViewController?
+
     private lazy var pullToRefresh: UIRefreshControl = {
         let pullToRefresh = UIRefreshControl()
         pullToRefresh.addTarget(self, action: #selector(refreshData), for: .valueChanged)
@@ -208,10 +210,15 @@ extension MessageListController: UICollectionViewDelegate, UICollectionViewDataS
         keyWindow.addSubview(focusedView)
         focusedView.addSubview(snapshotCell)
         blurView.frame = keyWindow.frame
-        UIView.animate(withDuration: 0.6, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseIn) { [weak self] in
+        
+        let message = viewModel.messages[indexPath.item]
+        attachMenuActionItems(to: message, in: keyWindow)
+        UIView.animate(withDuration: 0.6, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseIn) {
             blurView.alpha = 1
-            focusedView.center.y = keyWindow.center.y
+            focusedView.center.y = keyWindow.center.y - 60
             snapshotCell.frame = focusedView.bounds
+            
+            snapshotCell.layer.applyShadow(color: .gray, alpha: 0.2, x: 2, y: 2, blur: 4)
         }
         
         UIApplication.dismissKeyboard()
@@ -227,18 +234,53 @@ extension MessageListController: UICollectionViewDelegate, UICollectionViewDataS
         }
     }
     
+    private func attachMenuActionItems(to message: MessageItem, in window: UIWindow) {
+        guard let focusedView, let startingFrame else { return }
+        
+        let reactionPickerView = ReactionPickerView(message: message)
+        
+        let reactionHostVC = UIHostingController(rootView: reactionPickerView)
+        reactionHostVC.view.backgroundColor = .clear
+        reactionHostVC.view.translatesAutoresizingMaskIntoConstraints = false
+        
+        window.addSubview(reactionHostVC.view)
+        reactionHostVC.view.bottomAnchor.constraint(equalTo: focusedView.topAnchor, constant: 5).isActive = true
+        reactionHostVC.view.leadingAnchor.constraint(equalTo: focusedView.leadingAnchor, constant: 20).isActive = message.direction == .received
+        reactionHostVC.view.trailingAnchor.constraint(equalTo: focusedView.trailingAnchor, constant: -20).isActive = message.direction == .sent
+        
+        let messageMenuView = MessageMenuView(message: message)
+        
+        let messageMenuHostVC = UIHostingController(rootView: messageMenuView)
+        messageMenuHostVC.view.translatesAutoresizingMaskIntoConstraints = false
+        messageMenuHostVC.view.backgroundColor = .clear
+        window.addSubview(messageMenuHostVC.view)
+        messageMenuHostVC.view.topAnchor.constraint(equalTo: focusedView.bottomAnchor, constant: 0).isActive = true
+        
+        messageMenuHostVC.view.leadingAnchor.constraint(equalTo: focusedView.leadingAnchor, constant: 20).isActive = message.direction == .received
+        messageMenuHostVC.view.trailingAnchor.constraint(equalTo: focusedView.trailingAnchor, constant: -20).isActive = message.direction == .sent
+
+        self.reactionHostVC = reactionHostVC
+        self.messageMenuHostVC = messageMenuHostVC
+    }
+    
     @objc private func dismissContextMenu() {
-        UIView.animate(withDuration: 0.6, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseIn) {
-            self.focusedView?.frame = self.startingFrame ?? .zero
-            self.blurView?.alpha = 0
-        } completion: { _ in
-            self.highlightedCell?.alpha = 1
-            self.blurView?.removeFromSuperview()
-            self.focusedView?.removeFromSuperview()
+        UIView.animate(withDuration: 0.6, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseIn) { [weak self] in
+            guard let self else { return }
+            focusedView?.frame = self.startingFrame ?? .zero
+            reactionHostVC?.view.removeFromSuperview()
+            messageMenuHostVC?.view.removeFromSuperview()
+            blurView?.alpha = 0
+        } completion: { [weak self] _ in
+            guard let self else { return }
+            highlightedCell?.alpha = 1
+            blurView?.removeFromSuperview()
+            focusedView?.removeFromSuperview()
             
-            self.highlightedCell = nil
-            self.blurView = nil
-            self.focusedView = nil
+            highlightedCell = nil
+            blurView = nil
+            focusedView = nil
+            reactionHostVC = nil
+            messageMenuHostVC = nil
         }
     }
     
@@ -262,7 +304,18 @@ private extension UICollectionView {
     }
 }
 
+extension CALayer {
+    func applyShadow(color: UIColor, alpha: Float, x: CGFloat, y: CGFloat, blur: CGFloat) {
+        shadowColor = color.cgColor
+        shadowOpacity = alpha
+        shadowOffset = .init(width: x, height: y)
+        shadowRadius = blur
+        masksToBounds = false
+    }
+}
+
 #Preview {
     MessageListView(viewModel: ChatRoomViewModel(.placeholder))
         .ignoresSafeArea()
+        .environmentObject(VoiceMessagePlayer())
 }
