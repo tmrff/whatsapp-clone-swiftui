abedf5c5fc96550eccfc9e0e7654cf7c48a09e2e
Thomas <thomasdroff@gmail.com>
2024-09-22 21:50:20 +1200

    Setup project for push notifications

--- WhatsAppClone/WhatsAppCloneApp.swift
+++ WhatsAppClone/WhatsAppCloneApp.swift
@@ -12,7 +12,46 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         FirebaseApp.configure()
+        setupPushNotifications(for: application)
         return true
+    }
+  
+    private func setupPushNotifications(for application: UIApplication) {
+        let notificationCenter = UNUserNotificationCenter.current()
+        Messaging.messaging().delegate = self
+        notificationCenter.delegate = self
+        let options: UNAuthorizationOptions = [.alert, .badge, .sound]
+      
+        notificationCenter.requestAuthorization(options: options) { granted, error in
+            if let error {
+                print("APNS Failed to request authorization: \(error.localizedDescription)")
+                return
+            }
+          
+            if granted {
+                print("APNS authorization granted")
+                DispatchQueue.main.async {
+                    application.registerForRemoteNotifications()
+                }
+            } else {
+              print("APNS authorization denied")
+            }
+        }
+    }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate, MessagingDelegate {
+  
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("APNS firebase device token: \(String(describing: fcmToken))")
+    }
+  
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print("APNS successfully registered with device token: \(deviceToken)")
+    }
+  
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification) async -> UNNotificationPresentationOptions {
+        return [.sound, .banner, .badge]
     }
 }
 
