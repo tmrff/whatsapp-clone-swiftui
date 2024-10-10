398290ee642ad443d718e14b43e85bbf890079c3
tmrff <thomasdroff@gmail.com>
2024-10-10 23:14:49 +1300

    Decode fcm token to UserItem

--- WhatsAppClone/Screens/Auth/Models/UserItem.swift
+++ WhatsAppClone/Screens/Auth/Models/UserItem.swift
@@ -13,6 +13,7 @@ struct UserItem: Identifiable, Hashable, Decodable {
     let email: String
     var bio: String?
     var profileImageURL: String? = nil
+    var fcmToken: String?
     
     var id: String {
         return uid
@@ -45,6 +46,7 @@ extension UserItem {
         self.email = dictionary[.email] as? String ?? ""
         self.bio = dictionary[.bio] as? String ?? nil
         self.profileImageURL = dictionary[.profileImageURL] as? String ?? nil
+        self.fcmToken = dictionary[.fcmToken] as? String ?? nil
     }
 }
 
@@ -54,4 +56,5 @@ extension String {
     static let email = "email"
     static let bio = "bio"
     static let profileImageURL = "profileImageUrl"
+    static let fcmToken = "fcmToken"
 }
--- WhatsAppClone/Screens/Auth/Services/AuthProvider.swift
+++ WhatsAppClone/Screens/Auth/Services/AuthProvider.swift
@@ -115,6 +115,7 @@ extension AuthManager {
             let loggedInUser = UserItem(dictionary: userDict)
             self?.authState.send(.loggedIn(loggedInUser))
             print("🔐 \(loggedInUser.username) is logged in")
+            print("🔐 \(loggedInUser.username)'s fcm token is \(loggedInUser.fcmToken)")
         } withCancel: { error in
             print("Failed to get current user info")
         }
