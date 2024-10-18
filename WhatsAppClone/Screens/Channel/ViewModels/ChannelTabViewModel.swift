d0c6e6855d5c5fc8abb478ab998c0bbd429745ec
tmrff <thomasdroff@gmail.com>
2024-10-18 19:58:35 +1300

    Cache channel members in memory

--- WhatsAppClone/Screens/Channel/ViewModels/ChannelTabViewModel.swift
+++ WhatsAppClone/Screens/Channel/ViewModels/ChannelTabViewModel.swift
@@ -37,7 +37,7 @@ final class ChannelTabViewModel: ObservableObject {
     
     private func fetchCurrentUserChannels() {
         guard let currentUid = Auth.auth().currentUser?.uid else { return }
-        FirebaseConstants.UserChannelsRef.child(currentUid).observe(.value) { [weak self] snapshot in
+        FirebaseConstants.UserChannelsRef.child(currentUid).queryLimited(toFirst: 12).observe(.value) { [weak self] snapshot in
             guard let dict = snapshot.value as? [String: Any] else { return }
             dict.forEach { key, value in
                 let channelId = key
@@ -54,15 +54,25 @@ final class ChannelTabViewModel: ObservableObject {
         FirebaseConstants.ChannelsRef.child(channelId).observe(.value) { [weak self] snapshot in
             guard let dict = snapshot.value as? [String: Any], let self = self else { return }
             var channel = ChannelItem(dict)
-            channel.members = []
-            self.getChannelMembers(channel) { members in
-                channel.members = members
+            
+            if let memCachedChannel = self.channelDictionary[channelId], !memCachedChannel.members.isEmpty {
+                // Later fetches
+                channel.members = memCachedChannel.members
                 channel.unreadCount = unreadCount
-                channel.members.append(self.currentUser)
                 self.channelDictionary[channelId] = channel
                 self.reloadData()
-//                self?.channels.append(channel)
-                print("channel: \(channel.title)")
+                print("channel members fetched from cache: \(channel.members.map { $0.username })")
+            } else {
+                // Initial fetch
+                self.getChannelMembers(channel) { members in
+                    channel.members = members
+                    channel.unreadCount = unreadCount
+                    channel.members.append(self.currentUser)
+                    self.channelDictionary[channelId] = channel
+                    self.reloadData()
+    //                self?.channels.append(channel)
+                    print("channel members fetched from firebase: \(channel.members.map { $0.username })")
+                }
             }
         } withCancel: { error in
             print("Failed to get the channel for id \(channelId): \(error.localizedDescription)")
@@ -73,6 +83,7 @@ final class ChannelTabViewModel: ObservableObject {
         guard let currentUid = Auth.auth().currentUser?.uid else { return }
         let channelMemberUids = Array(channel.membersUids.filter { $0 != currentUid }.prefix(2))
         UserService.getUsers(with: channelMemberUids) { userNode in
+            print("channel getChannelMembers: \(userNode.users.count)")
             completion(userNode.users)
         }
     }
